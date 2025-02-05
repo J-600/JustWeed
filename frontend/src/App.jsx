b4a34e12bcb580ee8be641ab5eb2d2a661b580c7
J100 <jhonpanora06@gmail.com>
2025-02-05 22:02:24 +0100

    loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [mail, setMail] = useState('');
   const [password, setPassword] = useState('');
   const [responseMessage, setResponseMessage] = useState(null);
+  const [islogged, setIslogged] = useState(null);
 
   const navigate = useNavigate();
 
@@ -19,22 +20,25 @@ function App() {
         const res = await fetch('http://localhost:3000/products', {
           credentials: 'include',
         });
-        
+
         if (res.ok) {
           const sessionRes = await fetch('http://localhost:3000/session', {
             credentials: 'include',
           });
-          
+
           if (sessionRes.ok) {
             const sessionData = await sessionRes.json();
-            navigate('/products', { 
-              state: { 
-                email: sessionData.email, 
-                username: sessionData.username 
-              } 
+            navigate('/products', {
+              state: {
+                email: sessionData.email,
+                username: sessionData.username
+              }
             });
+            
           }
+          
         }
+        setIslogged(true)
       } catch (error) {
         console.log('Not authenticated');
       }
@@ -47,7 +51,7 @@ function App() {
     e.preventDefault();
     const hashedPassword = CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex);
     // console.log("Hashed Password:", hashedPassword)
-    
+
     try {
       const res = await fetch('http://localhost:3000/login', {
         method: 'POST',
@@ -70,13 +74,17 @@ function App() {
       } catch (error) {
         setResponseMessage(data);
       }
-
+      
     } catch (error) {
       console.error('Errore durante la richiesta:', error);
       setResponseMessage('Errore durante la richiesta');
     }
   };
-  
+  if (islogged === null) {
+    return (<div className={styles.page}><TopBar></TopBar><div className='loaderContainer'>
+      <Loader />
+    </div></div>);
+  }
 
   return (
     <div className={styles.page}>
@@ -117,16 +125,16 @@ function App() {
                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                 clipRule="evenodd" />
             </svg>
-            <input type="password" className="grow focus:outline-none" placeholder="Password" 
+            <input type="password" className="grow focus:outline-none" placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              required/>
+              required />
           </label>
           <br />
           <div className={styles.buttonContainer}>
-          <button type="submit" className="btn btn-wide bg-[#79a9ec] border-[#79a9ec] text-white hover:bg-[#5c81b6] hover:border-[#5c81b6]">
-      Login
-    </button>
+            <button type="submit" className="btn btn-wide bg-[#79a9ec] border-[#79a9ec] text-white hover:bg-[#5c81b6] hover:border-[#5c81b6]">
+              Login
+            </button>
           </div>
         </form>
 
