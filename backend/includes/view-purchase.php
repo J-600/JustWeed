ef6993f7baba845c6f8a3ce11dc9719b0575d61e
J100 <jhonpanora06@gmail.com>
2025-02-12 23:12:45 +0100

    started view purchase

---
+++ backend/includes/view-purchase.php
@@ -0,0 +1,85 @@
+<?php
+
+try {
+
+    // if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    //     throw new Exception("Non è una richiesta POST");
+    // }
+
+    require_once "dbh.inc.php";
+
+    $table = "selled_jw";
+    $table_products = "products_jw";
+    $table_addresses = "addresses_jw";
+    $table_cards = "cards_jw";
+    $email = $_POST["email"] ?? "jhonpanora06@gmail.com";
+
+    $sql = "SELECT s.*,
+                    p.name as product_name,
+                    p.img,
+                    p.description as product_description,
+                    p.price as product_price,
+                    p.quantity as product_quantity,
+                    p.verified as product_verify,
+                    p.view as product_view,
+                    a.name as address_name,
+                    a.street as address_street,
+                    a.city as address_city,
+                    a.zip as address_zip,
+                    c.numero as card_number,
+                    c.scadenza as card_expiry,
+                    c.circuito as card_circuit,
+                    c.nome_titolare as card_nome
+            FROM $table s
+            JOIN $table_products p ON s.id_product = p.id
+            JOIN $table_addresses a ON s.id_address = a.id
+            JOIN $table_cards c ON s.id_payment = c.id
+            WHERE s.email = :email";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":email", $email);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    if(empty($result))
+        throw new Exception("Non sono presenti acquisti");
+    
+
+    foreach ($result as $key => $row) {
+        if (!empty($row["img"])) {
+            $result[$key]["img"] = "data:image/png;base64," . base64_encode($row["img"]);
+        } else {
+            $result[$key]["img"] = null;
+        }
+    }
+
+    $response = [
+        "response" => 200,
+        "message" => True,
+        "data" => $result
+    ];
+
+    
+
+    echo json_encode($response);
+
+}  catch (PDOException $e) {
+    $response = [
+        "response" => 500,
+        "message" => false,
+        "data" => "Errore del database: " . $e->getMessage()
+    ];
+    echo json_encode($response);
+} catch (Exception $e) {
+    $response = [
+        "response" => 200,
+        "message" => false,
+        "data" => $e->getMessage()
+    ];
+    echo json_encode($response);
+}
+
+$pdo = null;
+$stmt = null;
+
+exit();
