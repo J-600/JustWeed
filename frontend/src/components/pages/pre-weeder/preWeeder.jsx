93042c473ebd057d27980caef115fc5cf5c172f5
J100 <jhonpanora06@gmail.com>
2025-02-21 11:40:00 +0100

    added page

--- frontend/src/components/pages/pre-weeder/preWeeder.jsx
+++ frontend/src/components/pages/pre-weeder/preWeeder.jsx
@@ -6,21 +6,28 @@ import { loadStripe } from "@stripe/stripe-js";
 
 
 function PreWeeder() {
+
+    const navigate = useNavigate();
     return (
         <div className="min-h-screen bg-gradient-to-br from-[#0A1128] to-[#1E2633] flex flex-col">
             <TopBar />
-            <div className="hero pt-32">
-                <div className="hero-content flex-col lg:flex-row">
+            <div className="hero pt-32 justify-center items-center">
+                <div className="hero-content flex-col lg:flex-row ">
                     <img
-                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                        className="max-w-m rounded-lg shadow-2xl" />
+                        src="caps.jpg"
+                        alt="caps"
+                        className="w-[22em] h-[28em] object-cover rounded-lg shadow-2xl" />
                     <div className="">
                         <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 animate-gradient">Become a Weeder!</h1>
                         <p className="py-6 text-gray-400">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            Ciao! Se stai cercando nuove opportunità di collaborazione nel mondo dei prodotti legali, JustWeed è il partner ideale per te. Diventando nostro collaboratore, avrai l'opportunità di entrare a far parte di un settore in crescita, con un prodotto sicuro e legale, che risponde a una domanda sempre più alta. Offriamo un supporto completo per aiutarti a iniziare la tua attività, con un sistema di vendita semplice e vantaggioso. Con JustWeed, potrai accedere a una rete consolidata, a margini competitivi e a risorse per la tua crescita professionale. Se sei pronto a entrare nel mercato con noi, unisciti a JustWeed e inizia a vendere subito!
                         </p>
-                        <button className="btn btn-primary">Become a weeder</button>
+                        <button
+                            className="btn bg-gradient-to-r from-blue-500 to-purple-600 text-white border-none hover:from-blue-600 hover:to-purple-700 transform transition-all duration-300 hover:scale-105"
+                            onClick={() => navigate("/weeder")}
+                        >
+                            Diventa uno Weeder
+                        </button>
                     </div>
                 </div>
             </div>
