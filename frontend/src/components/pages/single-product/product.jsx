8138488c21107a107295e6c5917366a04f5802f3
J100 <jhonpanora06@gmail.com>
2025-03-11 10:59:57 +0100

    started page single product

--- frontend/src/components/pages/single-product/product.jsx
+++ frontend/src/components/pages/single-product/product.jsx
@@ -3,14 +3,19 @@ import { useLocation, useNavigate } from "react-router-dom";
 import TopBar from "../../navbar/topbar";
 import Loader from "../../loader/loader";
 
-function Product(){
+function Product() {
 
     return (
 
         <div className="min-h-screen bg-gradient-to-br from-[#0A1128] to-[#1E2633] flex flex-col">
             <TopBar />
+            <div className="w-full px-4 pt-20">
+                <div className="card bg-[#1E2633] shadow-2xl border border-blue-900/30">
+                    <div className="card-body space-y-4">
+                    </div>
 
-            
+                </div>
+            </div>
         </div>
     );
 
