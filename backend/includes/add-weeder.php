958b0f3ee0099dfc4092db4fa42cbf05dfa05b22
J100 <jhonpanora06@gmail.com>
2025-04-02 15:43:09 +0200

    weeder php

--- backend/includes/add-weeder.php
+++ backend/includes/add-weeder.php
@@ -10,32 +10,78 @@ try {
     $payment = $_POST["payment"];
     $nome = $_POST["nome"];
     $cognome = $_POST["cognome"];
+    $full_name = $nome + " " + $cognome;
     $city = $_POST["city"];
-    $cap = $_POST["cap"];
-    $address= $_POST["addess"];
+    $zip = $_POST["cap"];
+    $street = $_POST["addess"];
     $piva = $_POST["piva"];
     $cf = $_POST["cf"];
     $descrizione = $_POST["descrizione"];
 
-    $table = "users_jw";
+    $table = "weeder_jw";
+    $table_cards = "cards_jw";
+    $table_address = "addresses_jw";
 
-    $sql = "SELECT COUNT(*) $table WHERE email = :email AND type = 'customer'";
+    $sql = "SELECT * FROM $table WHERE codiceFiscale = :cf";
     $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":cf", $cf);
     $stmt->execute();
 
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if ($result[0] == 0){
-        throw new Exception("Account già registrato come seller");
-    }
-    $sql = "UPDATE $table SET type = 'seller'";
+    $weeder = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (!empty($weeder))
+        throw new Exception("utente già seller");
+
+    $sql = "SELECT id $table_cards WHERE metodo = :metodo";
     $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":metodo", $payment);
+    $stmt->execute();
+
+    $id_p = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (!empty($id_p))
+        throw new Exception("carta necessaria");   
+
+    $sql = "INSERT INTO $table (email, name, street, city, zip) VALUES (:email, :name, :street, :city, :zip)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":name", $full_name);
+    $stmt->bindParam(":street", $street);
+    $stmt->bindParam(":city", $city);
+    $stmt->bindParam(":zip", $zip);
+
+    $stmt->execute();
+
+    $sql = "SELECT id FROM $table_address WHERE name = :name AND email = :email AND street = :street AND city = :city AND zip = :zip";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":name", $full_name);
+    $stmt->bindParam(":street", $street);
+    $stmt->bindParam(":city", $city);
+    $stmt->bindParam(":zip", $zip);
+
+    $stmt->execute();
+
+    $id_addr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    $sql = "INSERT INTO $table (firstName, secondName, email, codiceFiscale, pt, bio, card, address) VALUES (:name, :cognome, :email, :cf, :pt, :bio, :card, :address)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":cognome", $cognome);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":cf", $cf);
+    $stmt->bindParam(":pt", $piva);
+    $stmt->bindParam(":bio", $descrizione);
+    $stmt->bindParam(":card", $id_p);
+    $stmt->bindParam(":address", $id_addr);
+
+    $stmt->execute();
+
     $stmt->execute();
 
     $response = [
         "response" => 200,
         "message" => true,
-        "data"=> "Account diventato Seller"
+        "data"=> "Becomed weeder"
     ];
 
     echo json_encode($response);
@@ -47,6 +93,7 @@ try {
         "data" => "Errore del database: " . $e->getMessage()
     ];
     echo json_encode($response);
+
 } catch (Exception $e) {
     $response = [
         "response" => 200,
