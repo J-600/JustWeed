a7c5b77c2ee6e7a64d28cad5f8ae51b8a295ca80
J100 <jhonpanora06@gmail.com>
2025-04-02 14:47:08 +0200

    fixed some bugs

--- frontend/src/components/pages/signup/signup.jsx
+++ frontend/src/components/pages/signup/signup.jsx
@@ -49,9 +49,9 @@ function Signup() {
     };
 
     return (
-        <div className="min-h-screen bg-gradient-to-br from-base-100 to-1E2633 flex flex-col items-center justify-center p-4">
+        <div className="min-h-screen bg-gradient-to-br from-base-100 to-base-200 flex flex-col items-center justify-center p-4">
             <div className="flex-grow flex items-center justify-center w-full">
-                <div className="card w-full max-w-md bg-1E2633 shadow-2xl border border-blue-900/30 transform transition-all duration-500 hover:scale-105">
+                <div className="card w-full max-w-md bg-base-200 shadow-2xl border border-blue-900/30 transform transition-all duration-500 hover:scale-105">
                     <div className="card-body space-y-6 p-8">
                         <h1 className="text-5xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 animate-gradient leading-normal">
                             Sign Up
@@ -106,9 +106,13 @@ function Signup() {
                                         xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 16 16"
                                         fill="currentColor"
-                                        className="h-4 w-4 opacity-70">
+                                        className="h-4 w-4 opacity-70"
+                                    >
                                         <path
-                                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                                            fillRule="evenodd"
+                                            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                            clipRule="evenodd"
+                                        />
                                     </svg>
                                     <input
                                         type={isPasswordVisible ? "text" : "password"}
